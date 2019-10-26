44b0fbfa47b3e69de5ecb1d54c88a99386ae2204
CrimsonTek <56491941+CrimsonTek@users.noreply.github.com>
2019-10-26 02:09:19 -0700

    Cleaned some code

--- Items/STAB.cs
+++ Items/STAB.cs
@@ -14,23 +14,6 @@ namespace TerraTyping.Items
 {
     public class STAB : GlobalItem
     {
-        public override void SetDefaults(Item item)
-        {
-            //if (Items.Type.ContainsKey(item.type))
-            //{
-            //    if (ArmorPlayer.typeHead.Item1 == ArmorPlayer.typeBody.Item1 && ArmorPlayer.typeBody.Item1 == ArmorPlayer.typeLegs.Item1 && ArmorPlayer.typeHead.Item2 == ArmorPlayer.typeBody.Item2 && ArmorPlayer.typeBody.Item2 == ArmorPlayer.typeLegs.Item2)
-            //    {
-            //        ArmorPlayer.typeSet = new Tuple<Element.Type, Element.Type>(ArmorPlayer.typeBody.Item1, ArmorPlayer.typeBody.Item2);
-            //    }
-            //    else
-            //    {
-            //        ArmorPlayer.typeSet = new Tuple<Element.Type, Element.Type>(Element.Type.normal, Element.Type.none);
-            //    };
-            //    float STAB = 1.0f; if (ArmorPlayer.typeSet.Item1 == Items.Type[item.type] || ArmorPlayer.typeSet.Item2 == Items.Type[item.type])
-            //        { STAB = 15000.0f; } // effects the stab damage multiplier
-            //    item.damage = (int)(STAB);
-            //}
-        }
         public override void ModifyWeaponDamage(Item item, Player player, ref float add, ref float mult, ref float flat)
         {
             if (Items.Type.ContainsKey(item.type))
