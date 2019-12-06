ffeadcbd9cd31d5f9c1c4f468a460a13d4fcbe34
CrimsonTek <56491941+CrimsonTek@users.noreply.github.com>
2019-12-06 01:44:58 -0700

    Fixed guide telling you your life regen.

--- Items/Guide.cs
+++ Items/Guide.cs
@@ -1,23 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Terraria;
-using Terraria.ID;
-using Terraria.ModLoader;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Terraria;
+//using Terraria.ID;
+//using Terraria.ModLoader;
 
-namespace TerraTyping
-{
-    public class Guide : GlobalNPC
-    {
-        public override void GetChat(NPC npc, ref string chat)
-        {
-            if (npc.type == NPCID.Guide)
-            {
-                Player player = Main.player[Player.FindClosest(npc.position, npc.width, npc.height)];
-                chat = ($"player.lifeRegen: " + player.lifeRegen);
-            }
-        }
-    }
-}
+//namespace TerraTyping
+//{
+//    public class Guide : GlobalNPC
+//    {
+//        public override void GetChat(NPC npc, ref string chat)
+//        {
+//            if (npc.type == NPCID.Guide)
+//            {
+//                Player player = Main.player[Player.FindClosest(npc.position, npc.width, npc.height)];
+//                chat = ($"player.lifeRegen: " + player.lifeRegen);
+//            }
+//        }
+//    }
+//}
