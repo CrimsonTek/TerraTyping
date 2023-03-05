e1cc92a25f0717136cdb1969c4d214bfd94c2bbf
CrimsonTek <Jack@DESKTOP-FJCPFGE>
2023-03-05 02:01:55 -0700

    slowly making progress

--- TerraTyping.cs
+++ TerraTyping.cs
@@ -28,98 +28,136 @@ namespace TerraTyping
 
         //public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)/* tModPorter Note: Removed. Use ModSystem.ModifyInterfaceLayers */
         //{
-            //    PlayerInput.SetZoom_Unscaled();
-            //    PlayerInput.SetZoom_MouseInWorld();
-            //    Rectangle rectangle1 = new Rectangle((int)((double)Main.mouseX + (double)Main.screenPosition.X), (int)((double)Main.mouseY + (double)Main.screenPosition.Y), 1, 1);
-            //    if ((double)Main.player[Main.myPlayer].gravDir == -1.0)
-            //        rectangle1.Y = (int)Main.screenPosition.Y + Main.screenHeight - Main.mouseY;
-            //    PlayerInput.SetZoom_UI();
-            //    IngameOptions.MouseOver();
-            //    IngameFancyUI.MouseOver();
+        //    PlayerInput.SetZoom_Unscaled();
+        //    PlayerInput.SetZoom_MouseInWorld();
+        //    Rectangle rectangle1 = new Rectangle((int)((double)Main.mouseX + (double)Main.screenPosition.X), (int)((double)Main.mouseY + (double)Main.screenPosition.Y), 1, 1);
+        //    if ((double)Main.player[Main.myPlayer].gravDir == -1.0)
+        //        rectangle1.Y = (int)Main.screenPosition.Y + Main.screenHeight - Main.mouseY;
+        //    PlayerInput.SetZoom_UI();
+        //    IngameOptions.MouseOver();
+        //    IngameFancyUI.MouseOver();
 
-            //    for (int index1 = 0; index1 < 200; ++index1)
-            //    {
-            //        if (Main.npc[index1].active)
-            //        {
-            //            Rectangle rectangle2 = new Rectangle((int)Main.npc[index1].Bottom.X - Main.npc[index1].frame.Width / 2,
-            //                                                    (int)Main.npc[index1].Bottom.Y - Main.npc[index1].frame.Height,
-            //                                                    Main.npc[index1].frame.Width,
-            //                                                    Main.npc[index1].frame.Height);
-            //            if (Main.npc[index1].type >= 87 && Main.npc[index1].type <= 92)
-            //                rectangle2 = new Rectangle((int)((double)Main.npc[index1].position.X + (double)Main.npc[index1].width * 0.5 - 32.0),
-            //                                            (int)((double)Main.npc[index1].position.Y + (double)Main.npc[index1].height * 0.5 - 32.0),
-            //                                            64,
-            //                                            64);
-            //            bool flag1 = rectangle1.Intersects(rectangle2);
-            //            bool flag2 = flag1 || Main.SmartInteractShowingGenuine && Main.SmartInteractNPC == index1;
-            //            if (flag2 && (
-            //                Main.npc[index1].type != 85 && 
-            //                Main.npc[index1].type != 341 && 
-            //                Main.npc[index1].aiStyle != 87 || 
-            //                Main.npc[index1].ai[0] != 0.0) && 
-            //                Main.npc[index1].type != 488)
-            //            {
-            //                if (flag1)
-            //                {
-            //                    float buffer = 4 * Main.UIScale;
-            //                    float prev = 0;
+        //    for (int index1 = 0; index1 < 200; ++index1)
+        //    {
+        //        if (Main.npc[index1].active)
+        //        {
+        //            Rectangle rectangle2 = new Rectangle((int)Main.npc[index1].Bottom.X - Main.npc[index1].frame.Width / 2,
+        //                                                    (int)Main.npc[index1].Bottom.Y - Main.npc[index1].frame.Height,
+        //                                                    Main.npc[index1].frame.Width,
+        //                                                    Main.npc[index1].frame.Height);
+        //            if (Main.npc[index1].type >= 87 && Main.npc[index1].type <= 92)
+        //                rectangle2 = new Rectangle((int)((double)Main.npc[index1].position.X + (double)Main.npc[index1].width * 0.5 - 32.0),
+        //                                            (int)((double)Main.npc[index1].position.Y + (double)Main.npc[index1].height * 0.5 - 32.0),
+        //                                            64,
+        //                                            64);
+        //            bool flag1 = rectangle1.Intersects(rectangle2);
+        //            bool flag2 = flag1 || Main.SmartInteractShowingGenuine && Main.SmartInteractNPC == index1;
+        //            if (flag2 && (
+        //                Main.npc[index1].type != 85 && 
+        //                Main.npc[index1].type != 341 && 
+        //                Main.npc[index1].aiStyle != 87 || 
+        //                Main.npc[index1].ai[0] != 0.0) && 
+        //                Main.npc[index1].type != 488)
+        //            {
+        //                if (flag1)
+        //                {
+        //                    float buffer = 4 * Main.UIScale;
+        //                    float prev = 0;
 
-            //                    Element Primary = new NPCWrapper(Main.npc[index1]).Primary;
-            //                    Element Secondary = new NPCWrapper(Main.npc[index1]).Secondary;
-            //                    Element Quatrinary = new NPCWrapper(Main.npc[index1]).Offensive;
+        //                    Element Primary = new NPCWrapper(Main.npc[index1]).Primary;
+        //                    Element Secondary = new NPCWrapper(Main.npc[index1]).Secondary;
+        //                    Element Quatrinary = new NPCWrapper(Main.npc[index1]).Offensive;
 
-            //                    var icon1 = GetTexture("Types/" + Formal.Name[Primary]);
-            //                    var icon2 = GetTexture("Types/" + Formal.Name[Secondary]);
-            //                    var icon4 = GetTexture("Types/" + Formal.Name[Quatrinary]);
+        //                    var icon1 = GetTexture("Types/" + Formal.Name[Primary]);
+        //                    var icon2 = GetTexture("Types/" + Formal.Name[Secondary]);
+        //                    var icon4 = GetTexture("Types/" + Formal.Name[Quatrinary]);
 
-            //                    int yOffset = 38;
-            //                    int xOffset = 12;
-            //                    Main.spriteBatch.Begin();
-            //                    int x = (int)((Main.mouseX + xOffset) * Main.UIScale);
-            //                    int y = (int)((Main.mouseY + yOffset) * Main.UIScale);
-            //                    if (Primary != Element.none && Primary != Element.levitate)
-            //                    {
-            //                        Main.spriteBatch.Draw(icon1, new Vector2(x + prev, y), null, Color.White, 0, new Vector2(0, 0), Main.UIScale, SpriteEffects.None, 0);
-            //                        prev += icon1.Width * Main.UIScale + buffer;
-            //                    }
-            //                    if (Secondary != Element.none && Secondary != Element.levitate)
-            //                    {
-            //                        Main.spriteBatch.Draw(icon2, new Vector2(x + prev, y), null, Color.White, 0, new Vector2(0, 0), Main.UIScale, SpriteEffects.None, 0);
-            //                    }
-            //                    if (Quatrinary != Element.none && Quatrinary != Element.levitate)
-            //                    {
-            //                        Main.spriteBatch.Draw(icon4, new Vector2(x, y + icon1.Width * Main.UIScale + buffer), null, Color.White, 0, new Vector2(0, 0), Main.UIScale, SpriteEffects.None, 0);
-            //                    }
-            //                    Main.spriteBatch.End();
-            //                    break;
-            //                }
-            //                break;
-            //            }
-            //        }
-            //    }
+        //                    int yOffset = 38;
+        //                    int xOffset = 12;
+        //                    Main.spriteBatch.Begin();
+        //                    int x = (int)((Main.mouseX + xOffset) * Main.UIScale);
+        //                    int y = (int)((Main.mouseY + yOffset) * Main.UIScale);
+        //                    if (Primary != Element.none && Primary != Element.levitate)
+        //                    {
+        //                        Main.spriteBatch.Draw(icon1, new Vector2(x + prev, y), null, Color.White, 0, new Vector2(0, 0), Main.UIScale, SpriteEffects.None, 0);
+        //                        prev += icon1.Width * Main.UIScale + buffer;
+        //                    }
+        //                    if (Secondary != Element.none && Secondary != Element.levitate)
+        //                    {
+        //                        Main.spriteBatch.Draw(icon2, new Vector2(x + prev, y), null, Color.White, 0, new Vector2(0, 0), Main.UIScale, SpriteEffects.None, 0);
+        //                    }
+        //                    if (Quatrinary != Element.none && Quatrinary != Element.levitate)
+        //                    {
+        //                        Main.spriteBatch.Draw(icon4, new Vector2(x, y + icon1.Width * Main.UIScale + buffer), null, Color.White, 0, new Vector2(0, 0), Main.UIScale, SpriteEffects.None, 0);
+        //                    }
+        //                    Main.spriteBatch.End();
+        //                    break;
+        //                }
+        //                break;
+        //            }
+        //        }
+        //    }
 
-            //    //PlayerInput.SetZoom_UI();
-            //    base.ModifyInterfaceLayers(layers);
+        //    //PlayerInput.SetZoom_UI();
+        //    base.ModifyInterfaceLayers(layers);
         //}
 
         public override void Load()
         {
-            Instance = this;
-
-            Static.Load();
-
-            Type[] arrType = Code.GetTypes();
-            foreach (Type type in arrType)
+            try
             {
-                LoadAttribute loadAttribute = type.GetCustomAttribute<LoadAttribute>();
-                if (loadAttribute != null)
-                {
-                    MethodInfo methodInfo = type.GetMethod("Load", BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
-                    methodInfo.Invoke(null, null);
-                }
-            }
+                Instance = this;
 
-            errors.Clear();
+                ElementHelper.Load();
+                ElementArray.Load();
+
+                Type[] arrType = Code.GetTypes();
+                foreach (Type type in arrType)
+                {
+                    LoadAttribute loadAttribute = type.GetCustomAttribute<LoadAttribute>();
+                    if (loadAttribute != null)
+                    {
+                        MethodInfo methodInfo = type.GetMethod("Load", BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+                        methodInfo.Invoke(null, null);
+                    }
+                }
+
+                errors.Clear();
+            }
+            catch (Exception e)
+            {
+                Logger.Error("Caught exception while loading TerraTyping.", e);
+            }
+        }
+
+        public override void PostSetupContent()
+        {
+            try
+            {
+                TypeLoader.Logger = Logger;
+                TypeLoader.IsLoadingTypes = true;
+
+                SpecialTooltip.StaticLoad();
+
+                NPCTypeLoader.Instance.SetupTypes();
+                WeaponTypeLoader.Instance.SetupTypes();
+                AmmoTypeLoader.Instance.SetupTypes();
+                ArmorTypeLoader.Instance.SetupTypes();
+                ProjectileTypeLoader.Instance.SetupTypes();
+                SpecialItemTypeLoader.Instance.SetupTypes();
+
+                ProjectileWrapper.PostSetupContent();
+
+                SpecialTooltip.Finish();
+
+                ElementArray.Clean();
+
+                TypeLoader.IsLoadingTypes = false;
+            }
+            catch (Exception e)
+            {
+                Logger.Error("Caught exception while loading TerraTyping.", e);
+            }
         }
 
         public override void Unload()
@@ -140,49 +178,23 @@ namespace TerraTyping
                 BuffUtils.addTypeBuffs = null;
                 BuffUtils.replaceTypeBuffs = null;
 
-                Static.Unload();
+                ProjectileWrapper.Unload();
+                ElementArray.Unload();
+                ElementHelper.Unload();
             }
             catch (Exception exception)
             {
                 Logger.Error("An error occured during the unloading process.", exception);
             }
 
+            SpecialTooltip.StaticUnload();
             Instance = null;
-        }
-
-        public override void PostSetupContent()
-        {
-            TypeLoader.Logger = Logger;
-            TypeLoader.IsLoadingTypes = true;
-
-            SpecialTooltip.StaticLoad();
-
-            //Enemies.SetupTypes();
-            NPCTypeLoader.Instance.SetupTypes();
-            //Items.SetupTypes();
-            WeaponTypeLoader.Instance.SetupTypes();
-            AmmoTypeLoader.Instance.SetupTypes();
-            
-            //Armors.SetupTypes();
-            ArmorTypeLoader.Instance.SetupTypes();
-            //Projectiles.SetupTypes();
-            ProjectileTypeLoader.Instance.SetupTypes();
-
-            //Items.LoadSpecialTooltips();
-
-            Static.PostSetupContent();
-
-            SpecialTooltip.Finish();
-
-            ElementArray.Clean();
-
-            TypeLoader.IsLoadingTypes = false;
         }
 
         private void Initialize<T>(
             Mod mod,
             ref Dictionary<string, T> _dict,
-            ref Dictionary<int, T> dict, 
+            ref Dictionary<int, T> dict,
             Func<string, Mod, int> getKey)
         {
             if (mod == null)
@@ -217,7 +229,8 @@ namespace TerraTyping
             }
         }
 
-        [Obsolete] private void Initialize(Mod mod, ref Dictionary<string, Element> _dict, ref Dictionary<int, Element> dict, bool projectile = false)
+        [Obsolete]
+        private void Initialize(Mod mod, ref Dictionary<string, Element> _dict, ref Dictionary<int, Element> dict, bool projectile = false)
         {
             if (mod == null)
                 return;
@@ -249,7 +262,8 @@ namespace TerraTyping
                 dict.Add(key, value);
             }
         }
-        [Obsolete] private void Initialize(Mod mod, ref Dictionary<string, Tuple<Element, Element>> _dict, ref Dictionary<int, Tuple<Element, Element>> dict, ref Dictionary<int, Tuple<Element, Element>> dict2)
+        [Obsolete]
+        private void Initialize(Mod mod, ref Dictionary<string, Tuple<Element, Element>> _dict, ref Dictionary<int, Tuple<Element, Element>> dict, ref Dictionary<int, Tuple<Element, Element>> dict2)
         {
             if (mod == null)
                 return;
@@ -286,7 +300,8 @@ namespace TerraTyping
                 }
             }
         }
-        [Obsolete] private void Initialize(Mod mod, ref Dictionary<string, Tuple<Element, Element, Element, Element>> _dict, ref Dictionary<int, Tuple<Element, Element, Element, Element>> dict)
+        [Obsolete]
+        private void Initialize(Mod mod, ref Dictionary<string, Tuple<Element, Element, Element, Element>> _dict, ref Dictionary<int, Tuple<Element, Element, Element, Element>> dict)
         {
             if (mod == null)
                 return;
